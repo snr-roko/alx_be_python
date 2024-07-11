5e4047bb25d46edc1e5cf64518f367ea491b2298
snr-roko <80484832+snr-roko@users.noreply.github.com>
2024-07-11 21:42:04 +0000

    test_simple_calculator.py and library_management.py both updated: coding styling updated to accomodate checker

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,50 +1,50 @@
 class Book :
-  def __init__ (self, title, author) :
+  def __init__(self, title, author) :
     self.title = title
     self.author = author
     self._is_checked_out = False
 
-  def check_out_book (self) :
+  def check_out_book(self) :
     if self._is_checked_out == False :
       self._is_checked_out = True
     else :
       print ("Book has already been checked out!!!")
 
-  def return_book (self) :
+  def return_book(self) :
     if self._is_checked_out == True :
       self._is_checked_out = False
     else :
       print("Book has not been checked out yet")
 
 
-  def check_out_status (self) :
+  def check_out_status(self) :
     if self._is_checked_out :
       return "unavailable"
     else :
       return "available"
 
 class Library :
-  def __init__ (self) :
+  def __init__(self) :
     self._books = []
   
-  def add_book (self, book) :
-    self._books.append (book)
+  def add_book(self, book) :
+    self._books.append(book)
     
   
-  def check_out_book (self, title) :
+  def check_out_book(self, title) :
     for book in self._books :
       if book.title == title :
-        return book.check_out_book ()
-    print (f"The Book {title} can not be found in the library")
+        return book.check_out_book()
+    print(f"The Book {title} can not be found in the library")
 
-  def return_book (self, title) :
+  def return_book(self, title) :
     self.title = title
     for book in self._books :
       if book.title == self.title :
         return book.return_book()
-    print ("Book Not Found")
+    print("Book Not Found")
 
-  def list_available_books (self) :
+  def list_available_books(self) :
     for book in self._books :
       if book.check_out_status() == "available" :
         print(f"{book.title} by {book.author}")
