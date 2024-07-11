9e1af6717196301006c463644ae1278c387d141a
snr-roko <80484832+snr-roko@users.noreply.github.com>
2024-07-11 21:37:56 +0000

    robust_division_calculator.py updated: space between float function and arguments removed to accomodate checker

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide (numerator, denominator) :
   try:
-    numerator = float (numerator)
-    denominator = float (denominator)
+    numerator = float(numerator)
+    denominator = float(denominator)
     result = numerator / denominator
     return f"The result of the division is {result}"
   except ValueError:
