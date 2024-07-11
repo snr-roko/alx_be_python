58ab13d2c1cff9a58a916978b2b9cbfa7d765a5d
snr-roko <80484832+snr-roko@users.noreply.github.com>
2024-07-11 21:46:06 +0000

    test_simple_calculator.py updated: test_add function changed test_addition function to accomodate checker, typo initiallly made in addition text

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_additio(self) :
+    def test_addition(self) :
         """Test the addition method."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
