154cf956cc11f44596e5ab514281f178f5e8295b
snr-roko <80484832+snr-roko@users.noreply.github.com>
2024-07-19 20:57:32 +0000

    Name replaced with title in instance attributes and all references in the instance methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,14 @@
 class Book:
-  def __init__(self, name, author, year):
-    self.name = name
+  def __init__(self, title, author, year):
+    self.title = title
     self.author = author
     self.year = year
 
   def __del__(self):
-    print(f"Deleting {self.name}")
+    print(f"Deleting {self.title}")
   
   def __str__(self):
-    return f"{self.name} by {self.author}, published in {self.year}"
+    return f"{self.title} by {self.author}, published in {self.year}"
 
   def __repr__(self):
-    return f"Book('{self.name}', '{self.author}', {self.year})"
+    return f"Book('{self.title}', '{self.author}', {self.year})"
