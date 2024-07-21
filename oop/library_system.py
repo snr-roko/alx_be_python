ecd3bafd514f83c7862213bfc53dbcc5c2150cac
snr-roko <80484832+snr-roko@users.noreply.github.com>
2024-07-21 22:00:52 +0000

    Library class updated: Instance method of add_book updated to reflect the instance method of books

--- oop/library_system.py
+++ oop/library_system.py
@@ -27,7 +27,7 @@ class Library:
   def __init__(self):
     self.books = []
   def add_book(self, book):
-    Library.books.append(book)
+    self.books.append(book)
 
   def list_books(self):
     for book in self.books:
